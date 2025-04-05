8b15c33e29b752e9b2b46d543d0b274ce3f88271
Cocomineral <tintinch59@gmail.com>
2025-04-05 05:06:17 -0400

    Tested UI and Mesh loading

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,46 @@
 use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_tweening::TweeningPlugin;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands : Commands)
-{
-    commands.spawn((Person, Name("Corentin Chrisment".to_string())));
-    commands.spawn((Person, Name("Boboch D Brew".to_string())));
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(TweeningPlugin)
+        .add_systems(Startup, (set_window_name, setup).chain())
+        .run();
 }
 
-fn hello_world()
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+    )
 {
-    println!("Hello world");
-}
-
-fn greet_people(query : Query<&Name, With<Person>>)
-{
-    for name in &query
+    let text_font = TextFont 
     {
-        println!("Hello {}", name.0);
-    }
+        font: asset_server.load("fonts/JetBrainsMono-Regular.ttf"),
+        font_size: 25.0,
+        ..default()
+    };
+
+    // --- Caméra 3D ---
+    commands.spawn((Camera3d::default(), Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y)));
+
+    // --- Une light ---
+    commands.spawn((PointLight::default(), Transform::from_xyz(4.0, 8.0, 4.0)));
+
+    // --- UI ---
+    commands.spawn(Node::default()).with_children(|parent| {
+        parent.spawn((Text::new("Hello from the UI!"), text_font));
+    });
+
+    // commands.spawn((SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/result.gltf"))), Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3 { x: 0.1, y: 0.1, z: 0.1 })));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
 }
 
 fn set_window_name(mut window_query: Query<&mut Window, With<PrimaryWindow>>)
@@ -31,12 +49,4 @@ fn set_window_name(mut window_query: Query<&mut Window, With<PrimaryWindow>>)
     {
         window.title = "Depths".to_string();
     }
-}
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (add_people, set_window_name))
-        .add_systems(Update, (hello_world, greet_people).chain())
-        .run();
 }
